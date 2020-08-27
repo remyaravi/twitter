f2e4169641a5473a454b37ee5a62c67ec22c5559
Remya R <68427908+remyaravi@users.noreply.github.com>
2020-08-27 18:03:14 +0530

    Create mainbot.py

---
+++ mainbot.py
@@ -0,0 +1,48 @@
+import tweepy
+import logging
+import os
+def create_api():
+    CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+    CONSUMER_SECRET = os.getenv('CONSUMER_SECRET ')
+    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        print('Error creating API', exc_info=True)
+        raise e
+    print('API created')
+    return api
+  
+  
+import time
+def emoji_follower_count(user):
+    emoji_numbers = {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
+                     4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣", 8: "8️⃣", 9: "9️⃣"}
+
+    follower_count_list = [int(i) for i in str(user.followers_count)]
+
+    emoji_followers = ''.join([emoji_numbers[k]
+                               for k in follower_count_list if k in emoji_numbers.keys()])
+
+    return emoji_followers # FOLLOWERS IN SMILEYS
+
+
+
+def main():
+    api = create_api()
+
+    while True:
+        # change to your own twitter_handle
+        user = api.get_user('the_ameen_manna')
+
+        api.update_profile(name=f'aMEEr|{emoji_follower_count(user)} Followers')
+        print(f'Updating twitter profile: {emoji_follower_count(user)}')
+        print("Waiting to refresh..")
+        time.sleep(60)
+
